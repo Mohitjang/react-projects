3d0c10f2f24635d7ca09b6654bdc23ce543938cd
Mohit Jangid <mjangidjj12@gmail.com>
2024-11-11 06:45:07 +0530

    added home page hero content

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import React from "react";
+import "./Home.css"
+import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum
+            quam nobis rem, recusandae eaque vitae deserunt aspernatur numquam
+            corporis magni quos necessitatibus amet vero, in veniam fugiat
+            voluptatem temporibus maiores! Dolore consequuntur inventore
+            recusandae!
+          </p> 
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="" />
+                Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+                More Info
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
